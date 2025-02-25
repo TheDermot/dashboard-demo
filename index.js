27c5a245451dc134304231e8c4b85b9d7e76d111
TheDermot <88741983+TheDermot@users.noreply.github.com>
2025-02-24 20:03:09 -0500

    Added close button to sidebar

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 const sidebarIcon = document.getElementById("hamburger-sidebar");
 const sidebar = document.querySelector(".sidebar");
+const closeSidebar = document.getElementById("close-sidebar");
+
+// Toggle sidebar with hamburger icon
 sidebarIcon.addEventListener("click", () => {
   sidebar.classList.toggle("active");
+});
+
+// Close sidebar with close button
+closeSidebar.addEventListener("click", () => {
+  sidebar.classList.remove("active");
 });
